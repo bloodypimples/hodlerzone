c046a9645a702dfb70ec5be5e931431da53fbdd0
Hieu <hieudoan2609@gmail.com>
2018-03-20 23:13:55 +0700

    use pg for production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,6 @@ Rails.application.configure do
 
   # for devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_cable.disable_request_forgery_protection = true
 end
